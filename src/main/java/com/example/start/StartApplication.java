f65f1f808f97796408076f515d77d50aad347ea7
Manuk-Han <aksdnr507@gmail.com>
2024-11-28 18:01:44 +0900

    multiplyTest

--- src/main/java/com/example/start/StartApplication.java
+++ src/main/java/com/example/start/StartApplication.java
@@ -13,15 +13,4 @@ public class StartApplication {
     public static void main(String[] args) {
         SpringApplication.run(StartApplication.class, args);
     }
-
-    @Test
-    public void multiplyTest() {
-        final int result = multiply(2, 3);
-
-        Assert.assertEquals(result, 5);
-    }
-
-    public int multiply(final int num1, final int num2) {
-        return 6;
-    }
 }
--- src/test/java/com/example/start/StartApplicationTests.java
+++ src/test/java/com/example/start/StartApplicationTests.java
@@ -1,13 +1,21 @@
 package com.example.start;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Assert;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class StartApplicationTests {
+public class StartApplicationTests {
 
     @Test
-    void contextLoads() {
+    public void multiplyTest() {
+        final int result = multiply(2, 3);
+
+        Assert.assertEquals(result, 6);
+    }
+
+    public int multiply(final int num1, final int num2) {
+        return 6;
     }
 
 }
